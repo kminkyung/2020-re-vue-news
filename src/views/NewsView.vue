a96477fd0dc5853400c281ed6aebf0556ef953fa
kminkyung <mminkyung.k@gmail.com>
2020-11-01 14:12:10 +0900

    first commit

---
+++ src/views/NewsView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="user in users">{{ user.title }}</div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        users: []
+      }
+    },
+    created() {
+      axios.get('https://api.hnpwa.com/v0/news/1.json') // Promise 기반 API
+        .then(response => this.users = response.data)
+        .catch(err => console.log(err))
+    }
+  }
+</script>
+
+<style></style>
